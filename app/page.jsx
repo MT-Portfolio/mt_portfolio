b47d031fd46573d358138dcf218344be1a7eb1a8
MT-JIONEX-Dev <156642099+MT-JIONEX-Dev@users.noreply.github.com>
2024-03-23 18:07:49 +0500

    fixed the error

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,5 @@
 import "../public/assets/css/style.css";
-import Home1 from "./(homes)/home-1/page";
 import Home2 from "./(homes)/home-2/page";
-import Home3 from "./(homes)/home-3/page";
 
 export const metadata = {
   title: "Preview || Mohammad Taha - Software Engineer Portfolio",
@@ -12,8 +10,6 @@ export default function page() {
   return (
     <>
       <Home2 />
-      {/* <Home1 />
-      <Home3 /> */}
     </>
   );
 }
