bd427ca2a564758c92508c4c0c78f04149eeb6ff
MT-JIONEX-Dev <156642099+MT-JIONEX-Dev@users.noreply.github.com>
2024-07-26 12:47:37 +0500

    Added knowledges and skills

--- data/knoledges.js
+++ data/knoledges.js
@@ -1,6 +1,7 @@
 export const knoledges = [
   "LAMP Stack",
   "MERN Stack",
+  "RoR",
   "PHP",
   "Laravel",
   "Yii",
--- data/skills.js
+++ data/skills.js
@@ -7,24 +7,30 @@ export const skillData = [
   },
   {
     id: 2,
+    title: "Ruby on Rails",
+    progress: 70,
+    progressClass: "progress-bg-1",
+  },
+  {
+    id: 3,
     title: "MERN Stack",
     progress: 90,
     progressClass: "progress-bg-2",
   },
   {
-    id: 3,
+    id: 4,
     title: "Web App",
     progress: 85,
     progressClass: "progress-bg-3",
   },
   {
-    id: 4,
+    id: 5,
     title: "Mobile App",
     progress: 85,
     progressClass: "progress-bg-4",
   },
   {
-    id: 4,
+    id: 6,
     title: "Databases",
     progress: 80,
     progressClass: "progress-bg-1",
